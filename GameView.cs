3e577e7987cce612d09669b88ff75f02ca15f7af
William <willspri@gmail.com>
2025-05-21 16:16:37 +1200

    started

---
+++ GameView.cs
@@ -0,0 +1,22 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Views;
+
+namespace visualMemory
+{
+    public class GameView : View
+    {
+        private Paint paint;
+
+        public GameView(Context context) : base(context)
+        {
+            paint = new Paint();
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            base.OnDraw(canvas);
+            canvas.DrawColor(Color.Black); 
+        }
+    }
+}
